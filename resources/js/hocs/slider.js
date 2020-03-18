4744f09e5ee585031fead55cddc1721e0f9be65f
Alice <alise-fox@inbox.ru>
2020-03-18 12:33:02 +0300

    WIP

--- resources/js/hocs/slider.js
+++ resources/js/hocs/slider.js
@@ -66,17 +66,15 @@ const SliderComponent = ({settings, data = [], customArrow = false}) =>
                     {
                         breakpoint: 1024,
                         settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3,
-                            infinite: true,
-                            dots: false
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
                         }
                     },
                     {
                         breakpoint: 600,
                         settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
+                            slidesToShow: 1,
+                            slidesToScroll: 1
                         }
                     },
                     {
@@ -100,17 +98,15 @@ const SliderComponent = ({settings, data = [], customArrow = false}) =>
                     {
                         breakpoint: 1024,
                         settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3,
-                            infinite: true,
-                            dots: false
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
                         }
                     },
                     {
                         breakpoint: 600,
                         settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
+                            slidesToShow: 1,
+                            slidesToScroll: 1
                         }
                     },
                     {
